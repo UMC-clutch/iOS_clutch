0e1d1de175a439f1d756bd03cf17845cb18ec02f
lyoodong <dwryu3079@naver.com>
2023-07-11 23:47:26 +0900

    ADD: SetFont 확장, 피그마 기준 폰트명으로 정의

---
+++ clutch_iOS/Source/Presenter/Global/SetFont/SetFont.swift
@@ -0,0 +1,35 @@
+//
+//  SetFont.swift
+//  clutch_iOS
+//
+//  Created by Dongwan Ryoo on 2023/07/11.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    struct Clutch {
+        //Headline
+        static let headtitlebold = UIFont.systemFont(ofSize: 26, weight: .bold)
+        
+        //Subheadline
+        static let subtitleBold = UIFont.systemFont(ofSize: 20, weight: .bold)
+        static let subtitleRegular = UIFont.systemFont(ofSize: 20, weight: .regular)
+        static let subheadBold = UIFont.systemFont(ofSize: 18, weight: .bold)
+        static let subheadMedium = UIFont.systemFont(ofSize: 18, weight: .medium)
+        static let subheadRegular = UIFont.systemFont(ofSize: 18, weight: .regular)
+        
+        //Body
+        static let baseBold = UIFont.systemFont(ofSize: 16, weight: .bold)
+        static let baseMedium = UIFont.systemFont(ofSize: 16, weight: .medium)
+        static let baseRegular = UIFont.systemFont(ofSize: 16, weight: .regular)
+        static let smallBold = UIFont.systemFont(ofSize: 14, weight: .bold)
+        static let smallMedium = UIFont.systemFont(ofSize: 14, weight: .medium)
+        static let smallRegular = UIFont.systemFont(ofSize: 14, weight: .regular)
+        static let tinyMedium = UIFont.systemFont(ofSize: 12, weight: .medium)
+        static let tinyRegular = UIFont.systemFont(ofSize: 12, weight: .regular)
+
+    }
+   
+}
