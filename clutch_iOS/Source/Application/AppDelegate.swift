10d66c81ed4967a0b0710305c8353bb5cc59517c
jiwoong264 <jiwoong264@gmail.com>
2023-07-27 17:42:53 +0900

    Feat: IQKeyboardManager 라이브러리 설치 및 적용 (키보드 처리)

--- clutch_iOS/Source/Application/AppDelegate.swift
+++ clutch_iOS/Source/Application/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true // 화면 아무 곳이나 터치 시 키보드 dismiss
+                
         return true
     }
 
