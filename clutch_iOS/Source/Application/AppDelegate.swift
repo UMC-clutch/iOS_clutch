8f5e65f77f5eeed9494b0db80c1b22627956c5bf
jiwoong264 <jiwoong264@gmail.com>
2023-08-02 15:49:56 +0900

    ADD: IQKeyboardManager 기능 추가 적용

--- clutch_iOS/Source/Application/AppDelegate.swift
+++ clutch_iOS/Source/Application/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true // 화면 아무 곳이나 터치 시 키보드 dismiss
         return true
     }
 
